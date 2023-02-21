8050137c93e9cf3e3c3918032ef2e76c14f06a61
Druglord <mcbondprince@yahoo.com>
2023-02-21 08:12:05 -1200

    Commit

--- src/main/java/inn/MultiThreads.java
+++ src/main/java/inn/MultiThreads.java
@@ -6,18 +6,5 @@ public class MultiThreads implements Runnable {
     @Override
 
         public void run() {
-        int value = 0;
-            while (true) {
-                // Perform task here
-                int result = value+=1;
-                Homepage.counterValue = result;
-                System.out.println(result);
-                try {
-                    Thread.sleep(1000); // sleep for 1 second
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
+    }
 }
